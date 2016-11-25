c9a61ae10ff3438a86ab0d42d80f080e626dbbd6
Sébastien Piquemal <sebpiq@gmail.com>
2016-11-26 00:10:58 +0200

    added https server to the examples

--- examples/openAllInCna.js
+++ examples/openAllInCna.js
@@ -1,20 +1,31 @@
+var https = require('https')
+var fs = require('fs')
+var path = require('path')
 var express = require('express')
 var CaptivePortal = require('../index').CaptivePortal
 var CnaIosClient = require('../index').CnaIosClient
 var CnaAndroidClient = require('../index').CnaAndroidClient
 CaptivePortal.clientClasses = [CnaAndroidClient, CnaIosClient]
 
-var app = express()
+// Start an HTTPS server with self-signed certificates
+// This server simply redirects to our http server
+var httpsApp = express()
+var httpsServer = https.Server({
+  key: fs.readFileSync(path.join(__dirname, './certs/server.key'), 'utf8'),
+  cert: fs.readFileSync(path.join(__dirname, './certs/server.crt'), 'utf8'),
+  passphrase: 'blabla',
+  requestCert: true
+}, httpsApp)
+httpsApp.get('*', function(req, res) { res.redirect('http://a.co') })
+httpsServer.listen(443, function() { console.log('https server listening') })
+
+// HTTP server with captive portal
+var httpApp = express()
 var captivePortal = new CaptivePortal()
-
-app.use(captivePortal.handler)
-
-app.get('/', function(req, res) { res.sendFile('home.html', { root: __dirname }) })
-app.get('/a', function(req, res) { res.sendFile('a.html', { root: __dirname }) })
-app.get('/b', function(req, res) { res.sendFile('b.html', { root: __dirname }) })
-
-app.use('/assets', express.static(__dirname + '/assets'))
-
-app.get('*', function(req, res) { res.redirect('/') })
-
-app.listen(80)
+httpApp.use(captivePortal.handler)
+httpApp.get('/', function(req, res) { res.sendFile('home.html', { root: __dirname }) })
+httpApp.get('/a', function(req, res) { res.sendFile('a.html', { root: __dirname }) })
+httpApp.get('/b', function(req, res) { res.sendFile('b.html', { root: __dirname }) })
+httpApp.use('/assets', express.static(__dirname + '/assets'))
+httpApp.get('*', function(req, res) { res.redirect('/') })
+httpApp.listen(80, function() { console.log('http server listening') })
